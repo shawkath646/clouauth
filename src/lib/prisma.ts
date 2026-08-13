cbed07999d3afd4e32cb1441175e482668180400
Shawkat Hossain Maruf <shawkath646@gmail.com>
2026-08-14 04:24:15 +0900

    Fixed env

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaMssql(process.env.LOCAL_DATABASE_URL!),
+    adapter: new PrismaMssql(process.env.DATABASE_URL!),
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
